968b09aa5793cea62ab3bbf262765027b43471c5
Jaimin Babariya <130003933+jaiminbabariya7@users.noreply.github.com>
2026-06-29 14:22:26 -0400

    chore: remove old files

--- code/DA - Customer Signup Analysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  EXTRACT(YEAR FROM signup_date) AS signup_year,
-  COUNT(customer_id) AS signup_count
-FROM
-  ecommerce_dataset.customers
-GROUP BY
-  signup_year
-ORDER BY
-  signup_year;
